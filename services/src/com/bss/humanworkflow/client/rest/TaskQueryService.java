7f1c2bdff8c26fd47d4cc7788374756ffdb50e5e
Manu Alvarez <manuel.alvarez@bpmsoasolutions.com>
2015-05-14 16:46:18 +0200

    adfAuthenticate: UMM tab

--- services/src/com/bss/humanworkflow/client/rest/TaskQueryService.java
+++ services/src/com/bss/humanworkflow/client/rest/TaskQueryService.java
@@ -14,6 +14,16 @@ import com.bss.security.JWTokens;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.faces.FactoryFinder;
+import javax.faces.context.FacesContext;
+
+import javax.faces.context.FacesContextFactory;
+import javax.faces.lifecycle.Lifecycle;
+import javax.faces.lifecycle.LifecycleFactory;
+
+import javax.security.auth.Subject;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import javax.servlet.http.HttpServletResponse;
@@ -35,6 +45,9 @@ import oracle.bpel.services.workflow.common.model.WorkflowContextType;
 import oracle.bpel.services.workflow.query.model.TaskListRequestType;
 import oracle.bpel.services.workflow.task.model.Task;
 
+import weblogic.security.URLCallbackHandler;
+import weblogic.security.services.Authentication;
+
 @Path("/TaskQueryService")
 public class TaskQueryService extends AbstractService {
     
@@ -43,7 +56,7 @@ public class TaskQueryService extends AbstractService {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   @NotAuthenticated
-  public Response authenticate(AuthenticateInput input, @Context HttpServletResponse res) {
+  public Response authenticate(AuthenticateInput input, @Context HttpServletResponse res, @Context HttpServletRequest req, @Context ServletContext context) {
     
     try {
       WorkflowContextType wf = getWorkflow().authenticate(input.getLogin(), input.getPassword());
@@ -57,6 +70,8 @@ public class TaskQueryService extends AbstractService {
       claims.put("AccessLevel", 1);
 
       String token = JWTokens.getToken(input.getLogin(), claims);
+        
+      adfAuthenticate(input.getLogin(), input.getPassword(), req);
       
       // Setup the cookies
       //res.addCookie(Utils.createCookie("eappu", input.getLogin()));
@@ -64,6 +79,7 @@ public class TaskQueryService extends AbstractService {
       
       return Response.ok().entity(wf).header("Authorization", "Bearer " + token).build();
     } catch(Exception e) {
+      e.printStackTrace();
       return WorkflowError.respond(400, "Bad request.");
     }    
     
@@ -100,6 +116,7 @@ public class TaskQueryService extends AbstractService {
         criteria = Criteria.getMyInitiatedTasks(userId);
       } 
       tasks = getWorkflow().queryTasks(token, criteria);
+
     } catch (Exception e) {
       return WorkflowError.respond(500, e.getMessage());
     }
@@ -138,6 +155,22 @@ public class TaskQueryService extends AbstractService {
     Task a = getWorkflow().getTaskDetailsById(token, taskId);
     return a;
   }
+  
+    private void adfAuthenticate(String _username, String _password , HttpServletRequest req){
+        
+        try {
+          Subject subject = null;
+
+          byte[] pw = _password.getBytes();
+          subject = Authentication.login(new URLCallbackHandler(_username, pw));
+          weblogic.servlet.security.ServletAuthentication.runAs(subject, req);
+          
+            
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+        
+    }
   
   
 }
